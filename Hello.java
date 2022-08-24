3de1378fc1c97beb7a653b963b45a8b2d6942e81
Paul11832 <111941735+Paul11832@users.noreply.github.com>
2022-08-23 22:39:06 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,14 @@
+
+public class Hello
+{ 
+	public static void main(String[] args)
+	{
+	
+System.out.println("<Paul Reid>");
+System.out.println("<Paul>");
+System.out.println("<Lab 1 creating a hello message>");
+System.out.println("<08/23/2022>");
+System.out.println("<CMSC 255 section 3>");
+System.out.println("<bruh>");
+	}
+}
